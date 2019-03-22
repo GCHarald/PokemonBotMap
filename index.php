3c99c5d2d9fa2bf85552972dc6ef84f2789cd7dc
Harald <GCHarald@users.noreply.github.com>
2019-03-22 15:43:17 +0100

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 		<link rel="stylesheet" href="https://unpkg.com/leaflet@1.3.1/dist/leaflet.css"
 			integrity="sha512-Rksm5RenBEKSKFjgI3a41vrjkw4EVPlJ3+OiI65vTjIdo9brlAacEuKOiQ5OFh7cOI1bkDwLqdLw3Zg0cRJAAQ==" crossorigin=""/>
    
-		<title>Raid Map</title>
+		<title>Raid Map Harburg</title>
       
     	<script src="https://unpkg.com/leaflet@1.3.1/dist/leaflet.js"
 			integrity="sha512-/Nsx9X4HebavoBvEBuyp3I7od5tA0UzAxs+j83KgC8PU0kgB4XiK4Lfe4y4cgBtaRJQEIFCW+oC506aPT2L1zw==" crossorigin=""></script>
